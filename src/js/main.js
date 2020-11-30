335174abb5674b60f25e24af955145ad3b2fab27
Maksymenko Daryna <maksimenkodaryna@gmail.com>
2020-11-30 21:38:35 +0200

    Updated index.html, style.css, main.js

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+var num = 12890; // number
+alert (num);
+var str = "Welcome to clinic site"; // string
+alert (str);
+var inf = 1/0; // infinity
+alert (inf);
+var nan = "abc"/2; // NaN
+alert (nan);
+var fal = 2 > 3; // false
+alert (fal);
+var tr = 2 < 3; // true
+alert (tr);
+var S; // undefined 
+alert (S);
+var services = ["Treatment", "Help", "Staff", "Doctors"] // array
+alert (services);
+var obj  = {clinic: "Best treatment"}; // object
+alert (obj.clinic);
+var nul = prompt("Please click Cancell or Esc (to see NULL)", 0); // null
+alert (nul);
+
+
+
+
+
